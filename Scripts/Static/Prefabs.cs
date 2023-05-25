bee16b95a086a27afefd2ae2a125edf4e43fd43c
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-05-25 15:59:34 -0500

    Create Prefabs.cs

---
+++ Scripts/Static/Prefabs.cs
@@ -0,0 +1,9 @@
+﻿namespace Inventory;
+
+public static class Prefabs
+{
+	public static PackedScene Inventory { get; } = Load("inventory");
+
+	private static PackedScene Load(string path) =>
+		GD.Load<PackedScene>($"res://Scenes/Prefabs/{path}.tscn");
+}
